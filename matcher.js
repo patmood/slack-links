eab87bafe13d2267962af397afbf2b5afa20d0b5
patmood <patrick.n.moody@gmail.com>
2016-02-19 08:47:29 -0800

    match protocol

--- matcher.js
+++ matcher.js
@@ -1,7 +1,21 @@
-
+const knownTLDs = [
+  'com',
+]
 
 module.exports = {
   testString: (str) => {
-    return false
+    // Match protocol
+    // match www
+    // match known tlds
+    const reg = new RegExp(
+      // '(^|[\s\n]|<br\/?>)' + // Start of link
+      '(https?|ftp)' +
+      ':\\/\\/' +
+      '([\\w\\-\\.]+)' +
+      '($|[\\s\\n]|<br\\/?>)' // end of link
+    , 'gi')
+    const match = str.match(reg)
+    
+    return match ? match[0] : null
   }
 }
--- tests/matcher.js
+++ tests/matcher.js
@@ -1,7 +1,30 @@
 const test = require('tape')
 const matcher = require('../matcher')
 
-test('matcher', (t) => {
+test('basic protocol', (t) => {
   t.plan(1)
-  t.equal(matcher.testString('abc'), false)
+  const expected = 'http://google.com'
+  const actual = matcher.testString('check out http://google.com')
+  t.equal(expected, actual)
 })
+
+test('secure protocol', (t) => {
+  t.plan(1)
+  const expected = 'https://google.com'
+  const actual = matcher.testString('check out https://google.com')
+  t.equal(expected, actual)
+})
+
+test('no protocol', (t) => {
+  t.plan(1)
+  const expected = 'google.com'
+  const actual = matcher.testString('check out google.com')
+  t.equal(expected, actual)
+})
+
+// test('known tld', (t) => {
+//   t.plan(1)
+//   const expected = 'google.com'
+//   const actual = matcher.testString('check out google.com')
+//   t.equal(expected, actual)
+// })

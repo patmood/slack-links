fe1437f5f29bd01f647a21d990b6d08fdd239494
patmood <patrick.n.moody@gmail.com>
2016-02-21 22:26:03 -0800

    move variables to options

--- index.js
+++ index.js
@@ -3,22 +3,18 @@ const linksFromString = require('./links-from-string')
 
 const lastFetch = 1
 const links = []
+const opts = {
+  token: 'xoxp-21485397347-21487726449-21489223078-866dafb21c',
+  channel: 'C0MEBU4NB',
+  oldest: 1,
+  pretty: 1,
+}
 
-const token = 'xoxp-21485397347-21487726449-21489223078-866dafb21c'
-const channel = 'C0MEBU4NB'
-const oldest = 1
-const pretty = 1
-
-const getHistory = (token, channel, oldest, pretty) => {
+const getHistory = (options) => {
   request({
     url: 'https://slack.com/api/channels.history',
     json: true,
-    qs: {
-      token,
-      channel,
-      oldest,
-      pretty,
-    },
+    qs: options || {},
   }, (err, response, body) => {
     if (err) throw err
     if (!body.ok) throw body.error
@@ -33,4 +29,4 @@ const getHistory = (token, channel, oldest, pretty) => {
   })
 }
 
-getHistory(token, channel, oldest, pretty)
+getHistory(opts)
